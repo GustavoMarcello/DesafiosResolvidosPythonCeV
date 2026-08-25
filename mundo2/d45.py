02fe7ba1af138183ded32e3121c909cf1edf56c7
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-08-25 07:26:00 -0300

    add color to final message d45

--- mundo2/d45.py
+++ mundo2/d45.py
@@ -20,18 +20,18 @@ escolha = str(input('Digite sua escolha: ')).upper()
 rival = choice(opcoes)
 
 if escolha == rival:
-    print(f'Você \033[30m{escolha}\033[m X \033[30m{rival}\033[m Rival - EMPATE')
+    print(f'Você \033[30m{escolha}\033[m X \033[30m{rival}\033[m Rival - \033[30mEMPATE\033[m')
 elif escolha == 'PEDRA' and rival == 'PAPEL':
-    print(f'Você \033[33m{escolha}\033[m X \033[34m{rival}\033[m Rival - VOCÊ PERDEU')
+    print(f'Você \033[33m{escolha}\033[m X \033[34m{rival}\033[m Rival - \033[31mVOCÊ PERDEU\033[m')
 elif escolha == 'PEDRA' and rival == 'TESOURA':
-    print(f'Você \033[33m{escolha}\033[m X \033[35m{rival}\033[m Rival - VOCÊ GANHOU')
+    print(f'Você \033[33m{escolha}\033[m X \033[35m{rival}\033[m Rival - \033[32mVOCÊ GANHOU\033[m')
 elif escolha == 'PAPEL' and rival == 'PEDRA':
-    print(f'Você \033[34m{escolha}\033[m X \033[33m{rival}\033[m Rival - VOCÊ GANHOU')
+    print(f'Você \033[34m{escolha}\033[m X \033[33m{rival}\033[m Rival - \033[32mVOCÊ GANHOU\033[m')
 elif escolha == 'PAPEL' and rival == 'TESOURA':
-    print(f'Você \033[34m{escolha}\033[m X \033[35m{rival}\033[m Rival - VOCÊ PERDEU')
+    print(f'Você \033[34m{escolha}\033[m X \033[35m{rival}\033[m Rival - \033[31mVOCÊ PERDEU\033[m')
 elif escolha == 'TESOURA' and rival == 'PEDRA':
-    print(f'Você \033[35m{escolha}\033[m X \033[33m{rival}\033[m Rival - VOCÊ PERDEU')
+    print(f'Você \033[35m{escolha}\033[m X \033[33m{rival}\033[m Rival - \033[31mVOCÊ PERDEU\033[m')
 elif escolha == 'TESOURA' and rival == 'PAPEL':
-    print(f'Você \035[33m{escolha}\033[m X \033[34m{rival}\033[m Rival - VOCÊ GANHOU')
+    print(f'Você \033[35m{escolha}\033[m X \033[34m{rival}\033[m Rival - \033[32mVOCÊ GANHOU\033[m')
 else:
     print('Escolha digitada inválida')

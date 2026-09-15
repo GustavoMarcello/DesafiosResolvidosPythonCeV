5c5a2802553413f8cbb463f3ff6d330ff97fcde3
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-09-14 22:13:32 -0300

    d25

--- mundo4/d25.py
+++ mundo4/d25.py
@@ -5,7 +5,7 @@ EXERCÍCIO D25 - Hernça Transportadora
 Crie um programa que:
 1. Contenha a classe abstrata Transporte contendo:
     - distância
-    - frete
+    - valorFrete
     - calcFrete()
 2. Crie classes filhas:
     - Moto 
@@ -18,3 +18,59 @@ Crie um programa que:
         - fator = 9.5
         - distancia máxima 10 km
 """
+from abc import ABC, abstractmethod
+
+class Transporte(ABC):
+    def __init__(self, distancia=float):
+        self.distancia = distancia
+        self.valorFrete = 0
+
+    @abstractmethod
+    def calcularFrete(self):
+        pass
+
+
+class Moto(Transporte):
+    def __init__(self, distancia):
+        super().__init__(distancia)
+        self.fator = 0.5
+
+    def calcularFrete(self):
+        self.valorFrete = self.distancia * self.fator
+        print(f'Valor do frete de Moto: R$ {self.valorFrete}')
+
+
+class Caminhao(Transporte):
+    def __init__(self, distancia):
+        super().__init__(distancia)
+        self.fator = 1.2
+
+    def calcularFrete(self):
+        if self.distancia < 50:
+            print('Distância menor que a mínima')
+        else:
+            self.valorFrete = self.distancia * self.fator
+            print(f'Valor do frete de Caminhão: R$ {self.valorFrete}')
+
+
+class Drone(Transporte):
+    def __init__(self, distancia):
+        super().__init__(distancia)
+        self.fator = 9.5
+
+    def calcularFrete(self):
+        if self.distancia > 10:
+            print('Distância maior que a máxima')
+        else:
+            self.valorFrete = self.distancia * self.fator
+            print(f'Valor do frete de Drone: R$ {self.valorFrete}')
+
+
+moto = Moto(20)
+moto.calcularFrete()
+
+caminhao = Caminhao(60)
+caminhao.calcularFrete()
+
+drone = Drone(8)
+drone.calcularFrete()

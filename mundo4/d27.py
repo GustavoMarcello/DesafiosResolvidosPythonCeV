d64f5d0a61408a92597e1cfd665c662f9a44f3d0
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-09-16 13:57:37 -0300

    update

--- mundo4/d27.py
+++ mundo4/d27.py
@@ -14,3 +14,54 @@ Crie um programa que:
     - Guerreiro 
     - Mago
 """
+
+from abc import ABC, abstractmethod
+from random import randint
+
+class Personagem(ABC):
+    def __init__(self, nome=str, vida=int, golpes=list, equipamentos=list):
+        self.nome = nome
+        self.vida = vida
+        self.golpes = golpes
+        self.equipamentos = equipamentos
+
+    def rolarD20(self):
+        return randint(1, 20)
+
+    def atacar(self):
+        pass
+
+    def receberDano(self):
+        pass
+
+    def usarPocao(self):
+        pass
+
+
+class Mago(Personagem):
+    def __init__(self, nome=str, vida=int, golpes=list):
+        super().__init__(nome, vida, golpes)
+
+    def fireball(self):
+        pass
+
+    def magicMissles(self):
+        pass
+
+
+class Gerreiro(Personagem):
+    def __init__(self, nome=str, vida=int, golpes=list):
+        super().__init__(nome, vida, golpes)
+
+    def ataqueDesarmado(self):
+        pass
+
+    def ataqueComArma(self):
+        pass
+
+    def arremecarArma(self):
+        pass
+
+    
+
+    

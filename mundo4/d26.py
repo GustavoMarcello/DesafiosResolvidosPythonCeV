25c50ced4a81cbebda823f00219c5c9003b6521d
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-09-16 12:47:12 -0300

    updates d26 e 27

--- mundo4/d26.py
+++ mundo4/d26.py
@@ -17,3 +17,42 @@ Crie um programa que:
         - horasTrabalhadas
     - Mensalista 
 """
+
+from abc import ABC, abstractmethod
+
+class Funcionario(ABC):
+    salarioMinimo = 1612
+    inss = 7.5
+
+    def __init__(self, nome=str):
+        self.nome = nome
+        self.salarioBruto = 0
+        self.salario = 0
+        self.salarioMinimo = 1612
+        self.inss = 7.5
+
+    @abstractmethod
+    def calcularSalario(self):
+        pass
+
+    def analisarSalario(self):
+        salarios_minimos = self.salario / self.salarioMinimo
+        return salarios_minimos
+
+
+class Horista(Funcionario):
+    def __init__(self, nome=str, valorHora=float):
+        super().__init__(nome, valorHora)
+        self.valorHora = valorHora
+
+    def calcularSalario(self):
+        return super().calcularSalario()
+    
+
+class Mensalista(Funcionario):
+    def __init__(self, nome=str):
+        super().__init__(nome)
+
+    def calcularSalario(self):
+        return super().calcularSalario()
+    
